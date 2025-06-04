60be9a517e8752dc7712ac640343c077559257c7
vanessaoliveros <ramirezdavid3411@gmail.com>
2025-06-03 21:56:21 -0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 import os
-from flask_wtf.csrf import CSRFProtect
+#from flask_wtf.csrf import CSRFProtect
 
 db = SQLAlchemy()
 
@@ -10,7 +10,7 @@ login_manager = LoginManager()
 
 def create_app():
     app = Flask(__name__)
-    #app.config['SECRET_KEY'] = os.urandom(24)
+    app.config['SECRET_KEY'] = os.urandom(24)
     app.config.from_object('config.Config')
     db.init_app(app)
     login_manager.init_app(app)
